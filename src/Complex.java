d23d04783a0dcfd17a883cc64597524bc1aea21b
student <evanlay@billericak12.com>
2020-02-10 12:23:01 -0500

    Complex Commit

---
+++ src/Complex.java
@@ -0,0 +1,12 @@
+public class Complex {
+    private double one;
+    private double two;
+    public Complex(double a){
+        one = a;
+        two = 0.0;
+    }
+    public Complex(double a, double b){
+        one = a;
+        two = b;
+    }
+}
